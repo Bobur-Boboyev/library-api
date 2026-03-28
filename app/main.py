20f2849d065487aad543da3dd47d3e078c07c2d0
djumanov <djumanovdev@gmail.com>
2026-03-28 17:52:49 +0500

    database config

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 
 from .database import engine, Base
-from .dependencies import get_db
 
 
 Base.metadata.create_all(engine)
-
-print(get_db())
 
 app = FastAPI(title="Library API")
